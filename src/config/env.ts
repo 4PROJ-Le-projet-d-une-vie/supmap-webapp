47ee0451f052d5186e82dd63c1aaccc5a68e3338
Ewennn <ewen.bosquet@gmail.com>
2025-05-10 20:26:15 +0200

    tech/ Fix webapp environment variables defined during build

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-    usersHost: import.meta.env.VITE_USERS_HOST,
-    incidentsHost: import.meta.env.VITE_INCIDENTS_HOST,
-    gisHost: import.meta.env.VITE_GIS_HOST,
+    usersHost: (window as any).__ENV__.VITE_USERS_HOST,
+    incidentsHost: (window as any).__ENV__.VITE_INCIDENTS_HOST,
+    gisHost: (window as any).__ENV__.VITE_GIS_HOST,
 };
